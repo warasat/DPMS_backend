2ecd7c4476c4ed5352d2d85d5c65cd2e7322ba0b
Warasast <warasatwarraich@gmail.com>
2025-07-04 01:58:08 +0500

    .

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -23,7 +23,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://mediconnect3132.netlify.app"],
     credentials: true,
   })
 );
